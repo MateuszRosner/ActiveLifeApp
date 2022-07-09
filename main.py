a7a4efc47a366a1fae7080b3a9e57a37b83c4916
mateusz.rosner <rosner.mateusz@gmail.com>
2022-07-09 14:51:31 +0200

    graph coded

--- main.py
+++ main.py
@@ -25,6 +25,33 @@ class MyWindow(Ui_MainWindow):
             bytesize=serial.EIGHTBITS,
             timeout=0.5)
 
+        # --------------- chart settings ---------------
+        self.chartData = QChart()
+        self.axis_y = QValueAxis()
+        self.axis_x = QDateTimeAxis()
+        self.axis_x.setFormat("hh:mm:ss")
+        self.axis_x.setTitleText("Time")
+        self.axis_y.setTitleText("Value")
+        
+        self.maxData = QLineSeries(self.MainWindow)
+        self.maxData.setName("EMG / BIOZ")
+
+        self.chartData.addSeries(self.maxData)
+        self.chartData.setAnimationOptions(QChart.NoAnimation)
+        self.chartData.setTitle("MAX30001")
+        self.chartData.legend().setVisible(True)
+        self.chartData.legend().setAlignment(Qt.AlignBottom)
+        self.chartData.addAxis(self.axis_y, QtCore.Qt.AlignLeft)
+        self.chartData.addAxis(self.axis_x, QtCore.Qt.AlignBottom)
+
+        self.maxData.attachAxis(self.axis_x)
+        self.maxData.attachAxis(self.axis_y)
+
+        self.Graph.setChart(self.chartData)
+
+        self.create_linechart()
+
+
     # --------------- signals - slots config ---------------
         self.pushButtonConnect.clicked.connect(self.openPort)
 
@@ -43,8 +70,31 @@ class MyWindow(Ui_MainWindow):
         ports = serial.tools.list_ports.comports()
 
         for port, desc, hwid in sorted(ports):
-                print("{}: {} [{}]".format(port, desc, hwid))
+                print("{}: {}".format(port, desc))
                 self.comboBoxPorts.addItem(port+" "+desc)
+
+    def getActualSettings(self):
+        pass
+
+    def setActualSettings(self):
+        pass
+
+    def clearGraph(self):
+        pass
+
+    def create_linechart(self):
+        timenow = QtCore.QDateTime.currentDateTime()
+
+        if self.maxData.count() == 0:
+            self.axis_x.setMin(timenow)
+
+        self.maxData.append(timenow.toMSecsSinceEpoch(), 10)
+        self.axis_x.setMax(timenow)
+
+        self.axis_y.setMax(self.axis_y.max())
+
+    def clearGraph(self):
+        self.maxData.clear()
 
 
 if __name__ == "__main__":
