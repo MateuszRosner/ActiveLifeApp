a32db816a29e09023becd2d425c52e1e311d525e
mateusz.rosner <rosner.mateusz@gmail.com>
2022-08-01 12:45:57 +0200

    limits

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ class MyWindow(Ui_MainWindow):
                     self.ser.flushInput()
                     return
 
-        if (value > 200) or (value < -200):
+        if (value > 300) or (value < -300):
             print("[INFO] overlimit...")
             return
 
